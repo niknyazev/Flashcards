78af5acf66e95ac415caba643a738865a10c2b5c
niknyazev <n.knyazev.dev@gmail.com>
2022-01-24 15:27:26 +0300

    Add activity indicator

--- Flashcards/Controllers/ImageChoicer/ImageChoicerCellViewController.swift
+++ Flashcards/Controllers/ImageChoicer/ImageChoicerCellViewController.swift
@@ -10,15 +10,23 @@ import UIKit
 class ImageChoicerCellViewController: UICollectionViewCell {
     
     @IBOutlet weak var webImage: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        webImage.image = nil
+    }
     
     func configure(with urlImage: String) {
-        
+        contentView.backgroundColor = .lightGray
         guard let url = URL(string: urlImage) else { return }
         URLSession.shared.dataTask(with: url) { (data, _, _) in
             guard let data = data else { return }
             let image = UIImage(data: data)
             DispatchQueue.main.async {
                 self.webImage.image = image
+                self.activityIndicator.isHidden = true
+                self.activityIndicator.stopAnimating()
             }
         }.resume()
     }
