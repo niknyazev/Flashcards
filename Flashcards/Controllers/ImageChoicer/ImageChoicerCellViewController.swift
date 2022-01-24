ce74f9891ba7f8935813eb83ecf85a490d254c02
niknyazev <n.knyazev.dev@gmail.com>
2022-01-24 18:05:44 +0300

    Add caching image

--- Flashcards/Controllers/ImageChoicer/ImageChoicerCellViewController.swift
+++ Flashcards/Controllers/ImageChoicer/ImageChoicerCellViewController.swift
@@ -19,15 +19,49 @@ class ImageChoicerCellViewController: UICollectionViewCell {
         contentView.backgroundColor = .lightGray
         //
         guard let url = URL(string: urlImage) else { return }
-        URLSession.shared.dataTask(with: url) { (data, _, _) in
-            guard let data = data else { return }
+        
+        if let image = getCachedImage(from: url) {
+            setupImage(image: image)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { (data, response, _) in
+            guard let data = data, let response = response else { return }
+            
+            if url != response.url {
+                return //TODO: check how it work
+            }
+            
+            self.storeToCache(response: response, data: data)
+            
             let image = UIImage(data: data)
             DispatchQueue.main.async {
-                self.webImage.image = image
-                self.activityIndicator.isHidden = true
-                self.activityIndicator.stopAnimating()
+                self.setupImage(image: image)
             }
         }.resume()
+    }
+    
+    private func setupImage(image: UIImage?) {
+        webImage.image = image
+        activityIndicator.isHidden = true
+        activityIndicator.stopAnimating()
+    }
+    
+    private func storeToCache(response: URLResponse, data: Data) {
+        guard let url = response.url else { return }
+        let request = URLRequest(url: url)
+        let cachedResponse = CachedURLResponse(response: response, data: data)
+        URLCache.shared.storeCachedResponse(cachedResponse, for: request)
+    }
+    
+    private func getCachedImage(from url: URL) -> UIImage? {
+        let request = URLRequest(url: url)
+        let response = URLCache.shared.cachedResponse(for: request)
+        
+        if let response = response {
+            return UIImage(data: response.data)
+        }
+        return nil
     }
     
 }
