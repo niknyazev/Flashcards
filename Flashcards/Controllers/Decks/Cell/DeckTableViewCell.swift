668fae4d93517f70bbd46dc3bcc08d02d76ce2d8
niknyazev <n.knyazev.dev@gmail.com>
2022-02-20 13:36:07 +0300

    Remove TODO label

--- Flashcards/Controllers/Decks/Cell/DeckTableViewCell.swift
+++ Flashcards/Controllers/Decks/Cell/DeckTableViewCell.swift
@@ -50,9 +50,7 @@ class DeckTableViewCell: UITableViewCell {
     }
     
     private func addProgressCircle(flashcardsLearned: Float, color: UIColor) {
-                
-        //TODO: figure out why dont work arcCenter: iconView.center. - this value is 45, 40
-        
+                        
         let center = CGPoint(x: iconView.frame.height / 2, y: iconView.frame.width / 2)
         let radius = iconView.frame.height / 2 - 10
         
